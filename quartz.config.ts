ec1222cb87dec7f41ca873a5bea476bc6353c08e
Terry Fu <terryfu33@gmail.com>
2024-12-15 18:12:50 -0700

    Quartz sync: Dec 15, 2024, 6:12 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -31,7 +31,7 @@ const config: QuartzConfig = {
       colors: {
         lightMode: {
             
-          light: "#faf8f8",
+          light: "#ece2d1",
           lightgray: "#e5e5e5",
           gray: "#b8b8b8",
           darkgray: "#4e4e4e",
@@ -43,7 +43,7 @@ const config: QuartzConfig = {
         
         },
         darkMode: {
-          light: "#161618",
+          light: "#141d2c",
           lightgray: "#393639",
           gray: "#646464",
           darkgray: "#d4d4d4",
